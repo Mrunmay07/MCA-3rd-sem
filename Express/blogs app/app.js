a0c5be3e66bb23a80d0e2dacb54b0512ca122fc8
mrunmay-24-ukey <mrunmayukey587@gmail.com>
2026-08-20 11:58:22 +0530

    learn : Update Blog , like blog & delete blog

--- Express/blogs app/app.js
+++ Express/blogs app/app.js
@@ -1,28 +1,27 @@
 import express from "express";
 import blogsData from "./blogsDB.json" with { type: "json" };
-import crypto from "crypto"
-import multer from "multer"
-import path from 'path'
+import crypto from "crypto";
+import multer from "multer";
+import path from "path";
 import { writeFile } from "fs/promises";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-// Multer 
+// Multer
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    const id = crypto.randomUUID()
-    const extension = path.extname(file.originalname)
-   cb(null , `${id}${extension}`)
-  }
-})
+    const id = crypto.randomUUID();
+    const extension = path.extname(file.originalname);
+    cb(null, `${id}${extension}`);
+  },
+});
 
-const upload = multer({ storage: storage })
-
+const upload = multer({ storage: storage });
 
 // GET blogs
 app.get("/blogs", (req, res) => {
@@ -61,37 +60,99 @@ app.get("/blogs/:id", (req, res) => {
 });
 
 // Create a Blog
-app.post("/blogs" , upload.single('image') ,async (req , res) => {
-  const {title , content , author} = req.body
+app.post("/blogs", upload.single("image"), async (req, res) => {
+  const { title, content, author } = req.body;
 
-  if(!title || !content || !author){
-    return res.status(400).json({message : "All fields are requried"})
+  if (!title || !content || !author) {
+    return res.status(400).json({ message: "All fields are requried" });
   }
 
-  const blogId = crypto.randomUUID()
+  const blogId = crypto.randomUUID();
 
   const newBlog = {
     id: blogId,
     title,
     content,
     author,
-    likes : 0,
-    comments : [],
+    likes: 0,
+    comments: [],
     image: req.file ? `/uploads/${req.file.filename}` : null,
-    createdAt:new Date().toISOString(),
-    updatedAt : new Date().toISOString()
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+
+  blogsData.push(newBlog);
+
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Blog created successfully" });
+  } catch (err) {
+    return res.status(401).json({ message: err });
+  }
+});
+
+// Likes
+app.post("/blogs/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  const blog = blogsData.find((blog) => blog.id === id);
+
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
   }
 
-  blogsData.push(newBlog)
+  blog.likes += 1;
 
- try {
-   await writeFile("./blogsDB.json" , JSON.stringify(blogsData , null , 2) )
-   return res.status(201).json({message : "Blog created successfully"})
- } catch (err) {
-    return res.status(401).json({message : err })
- }
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Liked a Blog" });
+  } catch (err) {
+    return res.status(400).json({ message: "Failed to like blog" });
+  }
+});
 
+// Unlike
+
+// update blog -> PATCH
+app.patch("/blogs/:id", async (req, res) => {
+  const { id } = req.params;
+  const blog = blogsData.find((blog) => blog.id === id);
+
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+
+  const { title, content, author } = req.body;
+
+  if (title !== undefined) blog.title = title;
+  if (content !== undefined) blog.content = content;
+  if (author !== undefined) blog.author = author;
+
+  blog.updatedAt = new Date().toISOString();
+
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Blog Updated" });
+  } catch (err) {
+    return res.status(400).json({ message: "Failed to Update a  blog" });
+  }
+});
+
+// Delete a blog -> DELETE
+app.delete("/blogs/:id" , async (req , res) => {
+  const {id} = req.params
+  const blogIndex = blogsData.findIndex((blog) => blog.id === id)
+
+  blogsData.splice(blogIndex, 1)
+
+   try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Deleted a blog" });
+  } catch (err) {
+    return res.status(400).json({ message: "Failed to delete a  blog" });
+  }
 })
+
+
 
 app.listen(7000, () => {
   console.log("Server started at http://localhost:7000");
