a25f5a92494adfbcbd31a97352b53868c8e58c95
mrunmay-24-ukey <mrunmayukey587@gmail.com>
2026-09-17 13:21:00 +0530

    learn : Add Session Schema in blog application

--- Express/blogs app/controllers/userController.js
+++ Express/blogs app/controllers/userController.js
@@ -1,13 +1,10 @@
 import User from "../models/User.js";
-import crypto from "node:crypto";
+import bcrypt from "bcrypt"
 
 export async function register(req, res) {
   const { name, email, password } = req.body;
 
-  const hashedPassword = crypto
-    .createHash("sha256")
-    .update(password)
-    .digest("hex");
+  const hashedPassword = await bcrypt.hash(password , 12)
 
   if (!name || !email || !password) {
     return res.json({ message: "All fields are required to register" });
@@ -22,25 +19,30 @@ export async function register(req, res) {
   await User.create({
     name,
     email,
-    password : hashedPassword,
+    password: hashedPassword,
   });
 
   return res.json({ message: "User registered" });
 }
 
 export async function login(req, res) {
-  const { email, password } = req.body;
+  const { email, password } = req.body; // 12345
   if (!email || !password) {
     return res.json({ message: "All fields are required" });
   }
 
   const user = await User.findOne({
     email,
-    password,
   });
 
   if (!user) {
     return res.json({ message: "Invalid credentails" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password , user.password ) 
+
+  if(!isPasswordValid){
+    return res.json({message : "Invalid password or credentials"})
   }
 
   res.cookie("uid", user._id, {
